a05768c9279d0ec22e7be79fa5f312ea7f0fade8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-10-31 13:18:27 -0400

    progress

--- sw/3wish.02/ex07.py
+++ sw/3wish.02/ex07.py
@@ -19,14 +19,21 @@ bldg1a = doc.addObject("Part::Box",   "bldg1a")
 bldg1b = doc.addObject("Part::Box",   "bldg1b")
 
 screen1 = doc.addObject("Part::Plane", "screen1") 
+screen2 = doc.addObject("Part::Plane", "screen2") 
 
 stage.Length     = stage.Width  = 32.
 bldg1a.Length    = bldg1a.Width = 28.; bldg1a.Height = 3.
 bldg1b.Length    = bldg1b.Width = 26.; bldg1b.Height = 3.
+screen1.Width    = 8
+screen1.Length   = 8. / 1.77
+screen2.Width    = screen1.Width
+screen2.Length   = screen1.Length
 
-stage.Placement  = App.Placement(App.Vector(-1,-1, 0), App.Rotation( 0, 0, 0))
-bldg1a.Placement = App.Placement(App.Vector( 0, 0, 0), App.Rotation( 0, 0, 0))
-bldg1b.Placement = App.Placement(App.Vector( 1, 1, 1), App.Rotation( 0, 0, 0))
+stage.Placement   = App.Placement(App.Vector(-1,   -1, 0), App.Rotation( 0, 0, 0))
+bldg1a.Placement  = App.Placement(App.Vector( 0,    0, 0), App.Rotation( 0, 0, 0))
+bldg1b.Placement  = App.Placement(App.Vector( 1,    1, 1), App.Rotation( 0, 0, 0))
+screen1.Placement = App.Placement(App.Vector( 3,    3, 5), App.Rotation( 0, 90, 0))
+screen2.Placement = App.Placement(App.Vector( 11.5, 3, 5), App.Rotation( 0, 90, 0))
 
 bldgCut1   = App.activeDocument().addObject("Part::Cut", "Bldg central void")
 bldgCut1.Base = bldg1a
