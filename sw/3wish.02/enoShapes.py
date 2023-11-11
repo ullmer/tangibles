5008d3062a6c9c0d49afe357e3f7d90528c026a8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-11-10 23:22:09 -0500

    progress

--- sw/3wish.02/enoShapes.py
+++ sw/3wish.02/enoShapes.py
@@ -5,16 +5,15 @@
 import pivy.coin as coin
 import traceback
 
-####################### enodia cube ########################
+####################### enodia shape ########################
 
-class enoCube:
+class enoShape:
   diffuseColor   = (1,1,1)
   emmissiveColor = (1,1,1)
   size           = (1,1,1)
   transparency   = 0.7
   node           = None
   materialNode   = None
-  cubeNode       = None
 
   translation     = (0,0,0)
   translationNode = None
@@ -24,19 +23,18 @@ class enoCube:
   def __init__(self, **kwargs):
     #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
-    self.buildCube()
+    self.buildShape()
 
   ############# get node #############
 
   def getNode(self): return self.node
 
-  ############# buildCube #############
+  ############# buildShape #############
 
-  def buildCube(self):
+  def buildShape(self):
     self.node            = coin.SoSeparator()
     self.translationNode = coin.SoTranslation()
     self.materialNode    = coin.SoMaterial()
-    self.cubeNode        = coin.SoCube()
 
     self.node.addChild(self.translationNode)
     self.node.addChild(self.materialNode)
@@ -48,9 +46,32 @@ class enoCube:
     self.materialNode.emissiveColor  = self.emissiveColor
     self.materialNode.transparency   = self.transparency
 
+####################### enodia cube ########################
+
+class enoCube(enoShape):
+  cubeNode       = None
+
+  ############# constructor #############
+
+  def __init__(self, **kwargs):
+    #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    self.buildShape()
+
+  ############# buildShape #############
+
+  def buildShape(self):
+    print("foo")
+    super(enoCube, self).buildShape()
+
+    print("bar")
+    cubeNode = coin.SoCube()
+    self.node.addChild(cubeNode)
+
     w, d, h = self.size
     self.cubeNode.width  = w
     self.cubeNode.height = h
     self.cubeNode.depth  = d
 
 ### end ###
+
