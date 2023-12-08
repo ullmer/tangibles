8fa3b4aae25ec868b807a3fcbff35c49f177f15f
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-12-08 17:24:44 -0500

    progress

---
+++ content/tei07/ex03.py
@@ -0,0 +1,48 @@
+#New functionality warmup
+#By Brygg Ullmer, Clemson University
+#Begun 2023-12-08
+
+import random, math
+import pygame
+from   enoThemesTgz    import *
+from   pgzEno          import *
+from   pgzero.builtins import Actor
+
+WIDTH  = 1024
+HEIGHT = 768
+TITLE  = 'Enodia interactivity experiment'
+BLACK  = (0, 0, 0)
+
+ette = enoThemesTgzEnsemble()
+a1 = ette.addTheme("foo",    3, 5, "tg01h2-theme", pos=(200, 100))
+a2 = ette.addTheme("swishy", 5, 9, "tg01h2-theme", pos=(200, 300))
+
+a1.textPrimary = 'foo'
+a2.textPrimary = 'supercalifragilistic'
+
+a1.primaryTextOffset = (-60, -40)
+
+######################### draw #########################
+
+def draw():
+  screen.fill(BLACK)
+  ette.draw(screen)
+
+touch_coords = {} # dictionary with coordinates of active touches
+def normalizePos(x,y): return (int(x*WIDTH), int(y*HEIGHT))
+
+############ fingerdown ##########
+
+def on_finger_down(finger_id, x, y):
+  pos    = touch_coords[finger_id] = normalizePos(x,y)
+  cursor = Actor(cursorFn, pos)
+  cursors[finger_id] = cursor
+
+  px, py = x*WIDTH, y*HEIGHT
+
+  eae.on_finger_down(finger_id, px, py)
+
+pgze = pgzEno(["multitouch"])
+pgze.go()
+
+### end ###
