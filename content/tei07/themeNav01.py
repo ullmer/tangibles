17960cda4aafba472b41b48b6c0f82e20395a25e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-12-08 19:29:49 -0500

    pr

--- content/tei07/themeNav01.py
+++ content/tei07/themeNav01.py
@@ -18,12 +18,9 @@ BLACK  = (0, 0, 0)
 
 winpos(0,0, WIDTH, HEIGHT) #provided by pgzEno
 
-etpe = enoThemePgzEnsemble()
-a1 = etpe.addTheme("foo",    3, 5, "tg01h2-theme", pos=(200, 100))
-a2 = etpe.addTheme("swishy", 5, 9, "tg01h2-theme", pos=(200, 300))
 
 numBrackets = 6
-x, y = 50, 300
+x, y = 40, 280
 dx   = 250
 
 eae = enoActorEnsemble()
@@ -37,11 +34,23 @@ c      = ec.tallyCountries()
 kwDict = ec.tallyKeywords()
 thPap  = ec.tallyThemes()
 
+x,  y  = 120,75
+dy     = 100
+y0     = y
+
+etpe = enoThemePgzEnsemble()
+
 for theme in thPap:
   papers = thPap[theme]
   kcount = len(ec.themesKeywords[theme])
   pcount = len(papers)
   print("%s: K%i P%i" % (theme, kcount, pcount))
+  etpe.addTheme(theme, kcount, pcount, "tg01h2-theme", pos=(x,y))
+  y += dy
+  if y > HEIGHT: y = y0; x += dx
+
+#a1 = etpe.addTheme("foo",    3, 5, "tg01h2-theme", pos=(200, 100))
+#a2 = etpe.addTheme("swishy", 5, 9, "tg01h2-theme", pos=(200, 300))
 
 ######################### draw #########################
 
