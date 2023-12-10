4bc06a58a5e7d47776ff0496bacc5f029ef0150d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-12-10 18:01:36 -0500

    progress

--- content/tei07/enoContent.pl
+++ content/tei07/enoContent.pl
@@ -2,13 +2,23 @@
 % Brygg Ullmer, Clemson University
 % Begun 2023-12-10
 
-%%% Return all contributions (both abbreviations and title) associated with a specific country %%%
+%%% Return all contributions (both abbreviations and title) associated with a specific country, continent, or keyword
 
 contributionFromCountry(Abbrev, Title, Country) :-
   contribution(Abbrev, Title, Authors, _),
   authorsFromCountry(Authors, Country).
 
-%%% See if any of authors from a specified country %%%
+contributionFromContinentAbbrev(Abbrev, Title, ContinentAbbrev) :-
+  contribution(Abbrev, Title, Authors, _),
+  authorsFromCountry(Authors, Country),
+  continent(_,_,ContinentAbbrev,CountryList),
+  member(Country, CountryList).
+
+contributionWithKeyword(Abbrev, Title, Keyword) :-
+  contribution(Abbrev, Title, _, Keywords),
+  member(Keyword, Keywords).
+
+%%% Determine if any of authors from a specified country %%%
 
 authorsFromCountry([AuthorHead|AuthorTail], Country) :-
   not(is_list(AuthorHead)), 
@@ -17,10 +27,10 @@ authorsFromCountry([AuthorHead|AuthorTail], Country) :-
 authorsFromCountry([AuthorHead|AuthorTail], Country) :-
   is_list(AuthorHead), 
   (authorsFromCountry(AuthorHead, Country);
-   authorsFromCountry(AuthorTail, Country)).
+   authorsFromCountry(AuthorTail, Country)), !.  %avoids N matches when N authors from same country
 
 authorFromCountry([AuthorHead|AuthorTail], Country) :-
-  not(is_list(Author)),
+  not(is_list(AuthorHead)),
   last(AuthorTail, Country).
 
 %%% end %%%
