a9194a2e7b1df01eb93394ea238ab468fbc24013
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-12-27 11:08:44 -0500

    progress

---
+++ sw/pololu-zumo.01/exMidi07.py
@@ -0,0 +1,28 @@
+# Import text dump from exMidi03, and attempt to make it more legible + actionable
+# Brygg Ullmer, Clemson University
+# Begun 2023-12-27
+
+#Example lines:
+
+#- [2148, [48]]
+#- [ 134, [60, 53, 57, 65, 58, 70]]
+#- [ 204, [70, 65]]
+#- [ 205, [62]]
+#- [ 204, [65]]
+
+import yaml, mido, time
+
+yfn = '3400themerrypheastevenritchie3.yaml'
+yf  = open(yfn, 'rt')
+yd  = yaml.safe_load(yf)
+
+mo  = mido.open_output()
+
+for row in yd:
+  delayVal, notes = row
+  time.sleep(delayVal/1000.)
+  for note in notes: mo.send(mido.Message('note_on', note=note))
+  
+### end ###
+
+
