ae00c037202a7663f7e32344144740714fe73289
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-12-27 13:05:20 -0500

    draw midi space

---
+++ sw/pololu-zumo.01/drawMidi01.py
@@ -0,0 +1,16 @@
+# Example of drawing MIDI events
+# Brygg Ullmer, Clemson University
+# Begun 2023-12-27
+
+midiValsPerOctave = 12
+pixelsPerVal      = 4
+midiValsTotal     = 127
+
+HEIGHT = midiValsTotal * pixelsPerVal
+WIDTH  = 1200
+
+######################## draw grid ########################
+
+def drawGrid():
+
+### end ###
