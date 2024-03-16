249e3215d4858edc11256a147824a4d5f252ee7d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-03-16 08:50:56 -0400

    progress

--- sw/acm-iui.2024/commonPriorKws02.py
+++ sw/acm-iui.2024/commonPriorKws02.py
@@ -2,6 +2,7 @@
 # Brygg Ullmer, Clemson University
 # Begun 2024-03-16
 
+import sqlite3 # https://docs.python.org/3/library/sqlite3.html
 import yaml
 
 yfn='posters-iui24.yaml'
@@ -19,10 +20,20 @@ for key in p: # iterate through papers
       if kwId not in keywordId2papers: keywordId2papers[kwId] = []
       keywordId2papers[kwId].append(key)
 
+## connect to database ##
+
+con   = sqlite3.connect("dl-iui.db3")
+cur   = con.cursor()
+
 ## search for commonalities ##
 
 for key in keywordId2papers:
   val = keywordId2papers[key]
-  if len(val) > 1: print(key, val)
+  if len(val) > 1: 
+
+    query = 'select keyword from keywords where id=%i;' % key
+    result  = cur.execute(query)
+    qresult = result.fetchone()
+    print(key, val, qresult)
 
 ### end ###
