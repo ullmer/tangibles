552319340262e31e891012b077fdf8998b91d1cf
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-03-15 23:21:38 -0400

    basic function operable

--- sw/acm-iui.2024/acmKwsByTiIds.py
+++ sw/acm-iui.2024/acmKwsByTiIds.py
@@ -4,11 +4,15 @@
 
 import sqlite3 # https://docs.python.org/3/library/sqlite3.html
 
+titleIds = [637, 638]
+ti  = str(titleIds)[1:-1]
+
 con   = sqlite3.connect("dl-iui.db3")
 cur   = con.cursor()
 query = """select k.id, k.keyword from titles as t, keywords as k, ti_kw as tk, ti_au as ta, authors as a where
-             t.id in (367, 471, 474, 792, 969, 1308, 1327, 1480, 2113, 2185, 2268)
-             and ta.ti_id = t.id and ta.au_id = a.id and tk.ti_id = t.id and tk.kw_id = k.id group by keyword;"""
+             t.id in (%s)
+             and ta.ti_id = t.id and ta.au_id = a.id and tk.ti_id = t.id and tk.kw_id = k.id group by keyword;""" % (ti)
+print(query)
 
 result  = cur.execute(query)
 qresult = result.fetchall()
