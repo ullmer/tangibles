50fd9d91c8d3ab940abe0573eac3227e5e782b98
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-03-15 22:29:12 -0400

    populating

--- sw/acm-iui.2024/sample-queries.sql
+++ sw/acm-iui.2024/sample-queries.sql
@@ -9,3 +9,13 @@ select * from titles as t, keywords as k, ti_kw as tk, ti_au as ta, authors as a
 select k.keyword from titles as t, keywords as k, ti_kw as tk, ti_au as ta, authors as a where a.lastn in ('Laney', 'Dewan') and
    ta.ti_id = t.id and ta.au_id = a.id and tk.ti_id = t.id and tk.kw_id = k.id group by keyword;
 
+select t.id from titles as t, authors as a, ti_au as ta where a.lastn in ('Laney', 'Dewan') and 
+                        ta.ti_id = t.id and ta.au_id = a.id group by t.id;
+
+select id, lastn, firstn from authors where lastn in
+   ('Desmond', 'Ashktorab', 'Pan', 'Johnson', 'Dugan', 'Cooper') order by lastn, firstn;
+
+select id, lastn, firstn from authors where lastn in
+   ('Simic', 'Singh', 'Partl', 'Veas', 'Sabol') order by lastn, firstn;
+
+   
