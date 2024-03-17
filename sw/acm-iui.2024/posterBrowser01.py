9ba19e28081fc5f8b87b5aca6fe5e98b2d3fc176
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-03-17 01:06:40 -0400

    progress

--- sw/acm-iui.2024/posterBrowser01.py
+++ sw/acm-iui.2024/posterBrowser01.py
@@ -2,46 +2,39 @@
 # Brygg Ullmer, Clemson University
 # Begun 2024-03-17
 
-WIDTH  =
-HEIGHT = 
+WIDTH  = 2160
+HEIGHT = 2160
 
 class posterBrowser:
   topBlockFn = 'full_res/top_block01'
   upperHlBox = 'full_res/upper_highlight_box'
 
   arrayDim = [8, 8]
-  panelWidth  = None
-  panelHeight = None
+
+  actors   = None
+
+  ######################## constructor ######################## 
 
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
 
-    self.imgSrc = Image.open(self.imgSrcFn)
-    #self.imgSrc.show()
-    self.panelWidth, self.panelHeight = self.imgSrc.size
-    self.tileWidth  = self.panelWidth /self.arrayDim[0]
-    self.tileHeight = self.panelHeight/self.arrayDim[1]
+    self.buildActors()
 
-  def extractPane(self, tileCol, tileRow):
-    x1 = self.tileWidth  * tileCol
-    y1 = self.tileHeight * tileRow
-    x2 = self.tileWidth  * (tileCol+1)
-    y2 = self.tileHeight * (tileRow+1)
+  ######################## constructActors ######################## 
 
-    self.targPane = self.imgSrc.crop((x1, y1, x2, y2))
-    return self.targPane
-    #self.targPane.show()
-  
-tfnPre = "gridMap03k5"
-srcFn  = tfnPre + '.png'
-tp = tiledPanel(imgSrcFn=srcFn)
+  def constructActors(self):
 
-for i in range(tp.arrayDim[0]):
-  for j in range(tp.arrayDim[1]):
-    targPane = tp.extractPane(i,j)
-    fn = tp.imgSrcFn; 
-    tfn = '%s/%i%i.png' % (tfnPre, i, j)
-    print(tfn)
-    targPane.save(tfn)
+  ######################## draw ######################## 
+
+  def draw(self):
+    for actor in self.actors: actor.draw()
+
+######################## main ######################## 
+ 
+pb = posterBrowser()
+
+def draw(): 
+  screen.clear()
+  pb.draw()
 
 ### end ###
