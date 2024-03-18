128c350234276ca7ef492a3d4f96245e7246e7bd
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-03-18 14:29:00 -0400

    progress

--- sw/acm-iui.2024/extractPriorTitlesKeywords.py
+++ sw/acm-iui.2024/extractPriorTitlesKeywords.py
@@ -16,15 +16,24 @@ yf   = open(yfn, 'rt')
 yd   = yaml.safe_load(yf)
 
 posters = yd['posters']
-print("P:", posters)
 for i in range(1,numPosters+1):
   p = posters[i]
   t = p['title']
-  print(i, t)
+  print(str(i) + ':')
+  print('  title: "%s"' % t)
 
-#titleIds  = [781, 1031, 1560, 1751, 2073, 2084, 2189]
-#ti  = str(titleIds)[1:-1]
-#
+  try:
+    key = 'priorPaperIds'
+    if key in p: 
+      priorPID = p[key]
+      print("P", priorPID)
+
+    key = 'priorKwIds'
+    if key in p: 
+      priorPID = p[key]
+      print("K", priorPID)
+  except: pass
+
 #query = """select k.id, k.keyword from titles as t, keywords as k, ti_kw as tk, ti_au as ta, authors as a where
 #             t.id in (%s)
 #             and ta.ti_id = t.id and ta.au_id = a.id and tk.ti_id = t.id and tk.kw_id = k.id group by keyword;""" % (ti)
