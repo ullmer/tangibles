2ac0e615a5ee7a605340fd9c9eb46879fc4abf82
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-05-20 14:34:11 -0400

    tweaks

--- content/urp.24/py/exWind01.py
+++ content/urp.24/py/exWind01.py
@@ -7,7 +7,11 @@ WIDTH, HEIGHT = 1920, 1080
 w       = Actor('wind21e')
 touched = {'current': None}
 
+#### draw ####
+
 def draw(): screen.clear(); w.draw()
+
+#### on mouse movement ####
 
 def on_mouse_move(rel):
   dx, dy = rel
@@ -15,12 +19,15 @@ def on_mouse_move(rel):
   if touched['current'] == 'wind':
     x1, y1 = w.pos
     x2, y2 = x1+dx, y1+dy
-    w.pos = (x2, y2)
+    w.pos  = (x2, y2)
+
+#### when touchpad/mouse pressed ####
 
 def on_mouse_down(pos): 
   if w.collidepoint(pos): touched['current'] = 'wind'
+
+#### when touchpad/mouse released ####
      
-def on_mouse_up():       touched['current'] = None
-     
+def on_mouse_up():        touched['current'] = None
 
 ### end ###
