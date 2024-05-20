403aabe2e7820e6171f4fa3d3b9cd1f51a96603d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-05-20 12:37:32 -0400

    first working state

--- content/urp.24/py/exWind01.py
+++ content/urp.24/py/exWind01.py
@@ -2,7 +2,25 @@
 # Brygg Ullmer, Clemson University
 # Begun 2024-05-20
 
-w = Actor('wind21e')
-draw: w.draw()
+WIDTH, HEIGHT = 1920, 1080
+
+w       = Actor('wind21e')
+touched = {'current': None}
+
+def draw(): screen.clear(); w.draw()
+
+def on_mouse_move(rel):
+  dx, dy = rel
+
+  if touched['current'] == 'wind':
+    x1, y1 = w.pos
+    x2, y2 = x1+dx, y1+dy
+    w.pos = (x2, y2)
+
+def on_mouse_down(pos): 
+  if w.collidepoint(pos): touched['current'] = 'wind'
+     
+def on_mouse_up():       touched['current'] = None
+     
 
 ### end ###
