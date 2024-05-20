c32dbffb19d19378d2287fd739caa16f3aa9f430
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-05-20 12:25:21 -0400

    trivial example

---
+++ content/urp.24/py/exWind01.py
@@ -0,0 +1,8 @@
+# Successive illustrative examples of local & distributed (second) Wind
+# Brygg Ullmer, Clemson University
+# Begun 2024-05-20
+
+w = Actor('wind21e')
+draw: w.draw()
+
+### end ###
