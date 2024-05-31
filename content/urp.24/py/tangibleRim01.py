cf78db0a4067f3d1bf6049a48ee49818f590a2d5
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-05-31 07:50:50 -0400

    progress

--- content/urp.24/py/tangibleRim01.py
+++ content/urp.24/py/tangibleRim01.py
@@ -6,14 +6,27 @@
 #https://github.com/jeff-dh/SolidPython/blob/master-2.0.0-beta-dev/solid2/examples/11-fonts.x.py
 
 from solid2 import text, register_font, set_global_viewport_translation
-import yaml
+import yaml, sys
 
 yfn = '../yaml/rim02.yaml' #refactor to command-line argument
 yf  = open(yfn, 'rt')
 yd  = yaml.safe_load(yf)
 print(yd)
 
-register_font("11-font/RichEatin.otf")
+try:
+  ydr = yd['rim']
+except:
+  print("rim not found in source yaml); sys.exit(-1)
+
+try:
+  fontSide = ydr['fonts']['side']
+  typeface = fontSide['face']
+  fontSize = fontSide['size']
+except:
+  print("problems accessing font data from source yaml); sys.exit(-1)
+
+register_font("fonts/" + typeface)
+
 #set_global_viewport_translation([700, 900, 200])
 
 #text(font="Rich Eatin'", text="blablub").save_as_scad()
