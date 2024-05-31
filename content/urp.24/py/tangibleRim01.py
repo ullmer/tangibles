867478155fc24b5e26844b401656df77107077f4
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-05-31 07:24:23 -0400

    placeholder

---
+++ content/urp.24/py/tangibleRim01.py
@@ -0,0 +1,15 @@
+# SolidPython2 synthesis of rim textual expression
+# Brygg Ullmer, Clemson University
+# Begun 2024-05-31
+
+#Builds upon:
+#https://github.com/jeff-dh/SolidPython/blob/master-2.0.0-beta-dev/solid2/examples/11-fonts.x.py
+
+from solid2 import text, register_font, set_global_viewport_translation
+
+register_font("11-font/RichEatin.otf")
+#set_global_viewport_translation([700, 900, 200])
+
+#text(font="Rich Eatin'", text="blablub").save_as_scad()
+
+### end ###
