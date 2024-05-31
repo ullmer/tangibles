8ae7eab2a760db40b281afb534c6f9970765a013
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-05-31 19:36:27 -0400

    progress

---
+++ content/urp.24/py/supporting/tangibleRim02.py
@@ -0,0 +1,100 @@
+# SolidPython2 synthesis of rim textual expression
+# Brygg Ullmer, Clemson University
+# Begun 2024-05-31
+
+#Builds upon:
+#https://github.com/jeff-dh/SolidPython/blob/master-2.0.0-beta-dev/solid2/examples/11-fonts.x.py
+
+#from solid2 import text, register_font, set_global_viewport_translation, cube, translate, rotate
+#from solid import *
+#from solid.utils import *
+
+from solid2 import *
+
+import yaml, sys, math, traceback
+
+verbose = False
+
+yfn = '../yaml/rim02.yaml' #refactor to command-line argument
+yf  = open(yfn, 'rt')
+yd  = yaml.safe_load(yf)
+#if verbose: print(yd)
+
+try:
+  ydr = yd['rim']
+except:
+  print("rim not found in source yaml"); sys.exit(-1)
+
+#################### extract text angles #################### 
+
+def extractTextAngles(ydr):
+  result = []
+  try:
+    angles = ydr['angles']
+
+    for term in angles:
+      if 'centroidA' in term:
+        centroidAngle = term['centroidA']
+        result.append(centroidAngle)
+  except:
+    print("problem in extraction of rim text angles"); 
+    traceback.print_exc(); #sys.exit(-1)
+  return result
+
+#################### extract text angles #################### 
+
+def synthTextAngles(angles, texts, ellipseWidth, ellipseHeight):
+  result = None
+
+  lenAng, lenTxts = len(angles), len(texts)
+  if lenAng != lenTxts:
+    print("synthTextAngles issue: length of lists angles and texts differs! Aborting."); sys.exit(-1)
+
+  for i in range(lenAng):
+    angle    = angles[i]
+    textStr  = texts[i]
+    textGeom = text(text=textStr) #, font=
+
+    angleRadians = math.radians(angle)
+    x   = ellipseWidth  * math.cos(angleRadians)
+    y   = ellipseHeight * math.sin(angleRadians)
+    len = math.hypot(x,y)
+
+    trTxt  = translate([len,0,0])(textGeom)
+    rotTxt = rotate(a=angle)(trTxt)
+
+    if result == None: result =  rotTxt
+    else:              result += rotTxt
+
+  return result
+
+#################### main #################### 
+
+try:
+  fontSide = ydr['fonts']['side']
+  typeface = fontSide['face']
+  fontSize = fontSide['size']
+except:
+  print("problems accessing font data from source yaml")
+  traceback.print_exc(); #sys.exit(-1)
+
+try:
+  ellipseDimensions = ydr['dimensions']['OD']
+  ellipseWidth, ellipseHeight = ellipseDimensions
+except:
+  print("problems accessing ellipse dimensions from source yaml")
+  traceback.print_exc(); sys.exit(-1)
+
+if verbose: print("fonts:", typeface, fontSize)
+
+angles = extractTextAngles(ydr)
+if verbose: print(angles)
+
+geom = synthCubicApprox(angles, ellipseWidth, ellipseHeight)
+print(geom)
+
+register_font("fonts/" + typeface)
+set_global_viewport_translation([700, 900, 200])
+text(font="Rich Eatin'", text="blablub").save_as_scad()
+
+### end ###
