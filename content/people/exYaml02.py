72da61da0dcb64c54f6e727bb3046118b07454bc
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-06-12 00:09:21 -0400

    working to summarize fields

--- content/people/exYaml02.py
+++ content/people/exYaml02.py
@@ -11,7 +11,19 @@ yfn = 'people01.yaml'
 yf  = open(yfn, 'rt')
 yd  = yaml.safe_load(yf)
 
-
 numPeople = len(yd)
+print("num people:", numPeople)
+
+categories = {}
+for personData in yd:
+  fields = personData.keys()
+  for field in fields: 
+    if field in categories: categories[field] += 1
+    else:                   categories[field]  = 1
+
+print("fields observed")
+for field in categories:
+  numObserved = categories[field]
+  print("field %s; \t num observed: %i" % (field, numObserved))
 
 ### end ###
