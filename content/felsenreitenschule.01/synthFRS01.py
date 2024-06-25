26c617260290904eb2af3e5c6bb4f7100a340923
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-06-24 23:05:24 -0400

    progress

--- content/felsenreitenschule.01/synthFRS01.py
+++ content/felsenreitenschule.01/synthFRS01.py
@@ -12,13 +12,13 @@ elDim = {'arch': [6. , 1.9, 5.], 'dentil': [1.34, .56, 2.5],
 perBoxGeoms  = {}; boxWidth = elDim['box'][0] + elDim['column'][0]
 
 for elName in ['column', 'dentil']: #create cubical masses for columns and dentils
-  w,h,d = elDim[elName]; perBoxGeoms[elName] = cube(w,h,d)
+  w,h,d = elDim[elName]; perBoxGeoms[elName] = cube([w,h,d])
 
 aw, ah, ad  = elDim['arch']                # Prep to carve the arch
 archCyl1    = cylinder(r=.5, h=ad)         # We'll excise this cylinder from the archCutter
 archCyl2    = scale([aw, ah, 1])(archCyl1) # scaling it per elDimensions
 archCutter1 = cube([aw, ah, ad*.9])        # and borrow the archs dimensions, with less depth
-archCutter2 = translate([0, ah/2., 0])(archCutter) #shifting it upwards by the radius
+archCutter2 = translate([0, ah/2., 0])(archCutter1) #shifting it upwards by the radius
 archCutter3 = archCutter2 - archCyl2       # now, Boolean-subtract the cylindrical void 
 
 outGeom = archCutter3
