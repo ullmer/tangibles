45d220bfb9890b925847c3a379ef64b0e9e4578e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-06-24 23:13:45 -0400

    progress

--- content/felsenreitenschule.01/synthFRS01.py
+++ content/felsenreitenschule.01/synthFRS01.py
@@ -17,13 +17,14 @@ for elName in ['column', 'dentil']: #create cubical masses for columns and denti
 aw, ah, ad  = elDim['arch']                # Prep to carve the arch
 archCyl1    = cylinder(r=.5, h=ad)         # We'll excise this cylinder from the archCutter
 archCyl2    = scale([aw, ah, 1])(archCyl1) # scaling it per elDimensions
-archCutter1 = cube([aw, ah, ad*.9])        # and borrow the archs dimensions, with less depth
-archCutter2 = translate([0, ah/2., 0])(archCutter1) #shifting it upwards by the radius
+archCutter1 = cube([aw*1.1, ah, ad*.9])        # and borrow the archs dimensions, with less depth
+archCutter2 = translate([-aw*1.1/2., 0, 0])(archCutter1) #shifting it appropriately
 archCutter3 = archCutter2 - archCyl2       # now, Boolean-subtract the cylindrical void 
 
 outGeom = archCutter3
 
-print(scad_render(outGeom))
+radialSegments = 25;     hdr = '$fn = %s;' % radialSegments # create a header for the export
+scad_render_to_file(outGeom, 'frs01.scad', file_header=hdr) # write the .scad file
 
 ### end ###
 
