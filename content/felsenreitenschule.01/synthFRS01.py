ac11a030fc9baee0aa2dfe1d645d675e194bf531
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-06-24 22:52:57 -0400

    renaming

--- content/felsenreitenschule.01/synthFRS01.py
+++ content/felsenreitenschule.01/synthFRS01.py
@@ -1,8 +1,8 @@
-# SolidPython example code for Salzburg Feltenreitenschule
+# SolidPython example code for illuminated Salzburg Feltenreitenschule
 # Brygg Ullmer, Clemson University
 # Written 2024-06-24
 
-from solid import *        # load in SolidPython/SCAD support code
+from solid import *  # load in SolidPython/SCAD support code; solid2?
       
 #dimensions in mm, mating to 144/m LED strip
 
@@ -11,8 +11,11 @@ elDim = {'arch': [6. , 1.9, 3.], 'dentil': [1.34, .56, 2.5],
 
 perBoxGeoms  = {}; boxWidth = elDim['box'][0] + elDim['column'][0]
 
-for elName in ['dentil', 'column']:
+for elName in ['column', 'dentil']: #create cubical masses for columns and dentils
   w,h,d = elDim[elName]; perBoxGeoms[elName] = cube(w,h,d)
+
+# Next, 
+cyl1 = cylinder
 
 
 
