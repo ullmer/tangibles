e0760795bdb5c4f41241da00693542a4cc254f21
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-03 21:55:55 -0400

    progress

--- content/felsenreitenschule.01/synthFRS03.py
+++ content/felsenreitenschule.01/synthFRS03.py
@@ -9,9 +9,16 @@ efrs = enoFRS()
 
 backGrid  = efrs.synthPortal2DArray(10, 5)
 sideGrid  = efrs.synthPortal2DArray(5,  5)
-sideGridL = efrs.spinObj(sideGrid)
 
-efrs.renderScad('frs03.scad', portalGrid)
+sideGridL1 = efrs.spinObj(0, 0, -90, sideGrid)
+sideGridR1 = efrs.spinObj(0, 0,  90, sideGrid)
+
+sideGridL2 = efrs.shiftObj(0, -20, 0, sideGridL1)
+sideGridR2 = efrs.shiftObj(0,  20, 0, sideGridR1)
+
+scene = backGrid + sideGridL2 + sideGridR2
+
+efrs.renderScad('frs03.scad', scene)
 
 ### end ###
 
