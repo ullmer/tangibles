b939d6221d3bbd706a246af3198509599d8a45b8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-03 22:19:46 -0400

    progress

--- content/felsenreitenschule.01/synthFRS03.py
+++ content/felsenreitenschule.01/synthFRS03.py
@@ -11,15 +11,18 @@ backGrid   = efrs.synthPortal2DArray(10, 5)
 sideGrid   = efrs.synthPortal2DArray( 5, 5)
 cornerGrid = efrs.synthPortal2DArray( 1, 5)
 
-sideGridL1 = efrs.spinObj(  0, 0, -90, sideGrid)
-sideGridL2 = efrs.shiftObj( -8, -12.2, 0, sideGridL1)
-sideGridR2 = efrs.shiftObj( 90,   0, 0, sideGridL2)
+sideGridL1 = efrs.spinObj(  0,   0, -90, sideGrid)
+sideGridL2 = efrs.shiftObj(-8, -12.2, 0, sideGridL1)
+sideGridR2 = efrs.shiftObj(83,   2,   0, sideGridL2)
 
-cornerGridL1 = efrs.spinObj(0, 0, 45, cornerGrid)
+cornerGridL1 = efrs.spinObj(  0,  0,  45, cornerGrid)
 cornerGridL2 = efrs.shiftObj(-7, -4.2, 0, cornerGridL1)
 
+cornerGridR1 = efrs.spinObj(  0,  0, -45, cornerGrid)
+cornerGridR2 = efrs.shiftObj(71, -3.2, 0, cornerGridR1)
+
 scene  = backGrid + sideGridL2 + sideGridR2 
-scene += cornerGridL2
+scene +=          cornerGridL2 + cornerGridR2
 
 efrs.renderScad('frs03.scad', scene)
 
