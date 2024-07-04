6184cf89d900c81404873979faf369f05cd87cd5
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-03 22:46:28 -0400

    progress

--- content/felsenreitenschule.01/synthFRS04.py
+++ content/felsenreitenschule.01/synthFRS04.py
@@ -7,18 +7,18 @@ from enoFRS import *
 
 efrs = enoFRS()
 
-th = 4
+th = 4 #thickness, in mm
 
-backGrid   = efrs.synthPortal2DArrayHoles(10, 5, th, 5)
+backGrid   = efrs.synthPortal2DArrayHoles(10, 5, th,  5)
 sideGrid   = efrs.synthPortal2DArrayHoles( 5, 5, th,  0)
 cornerGrid = efrs.synthPortal2DArrayHoles( 1, 5, th, -4)
 
 sideGridL1 = efrs.spinObj(  0,   0, -90, sideGrid)
-sideGridL2 = efrs.shiftObj(-8, -12.2, 0, sideGridL1)
+sideGridL2 = efrs.shiftObj(-11, -16, 0, sideGridL1)
 sideGridR2 = efrs.shiftObj(83,   2,   0, sideGridL2)
 
 cornerGridL1 = efrs.spinObj(  0,  0,  45, cornerGrid)
-cornerGridL2 = efrs.shiftObj(-7, -4.2, 0, cornerGridL1)
+cornerGridL2 = efrs.shiftObj(-11, -4.7, 0, cornerGridL1)
 
 cornerGridR1 = efrs.spinObj(  0,  0, -45, cornerGrid)
 cornerGridR2 = efrs.shiftObj(71, -3.2, 0, cornerGridR1)
