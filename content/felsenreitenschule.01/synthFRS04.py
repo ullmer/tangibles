10ad6855e871b2466e5351fc41b2b76ace73b35d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-03 22:54:27 -0400

    progress

--- content/felsenreitenschule.01/synthFRS04.py
+++ content/felsenreitenschule.01/synthFRS04.py
@@ -15,13 +15,13 @@ cornerGrid = efrs.synthPortal2DArrayHoles( 1, 5, th, -4)
 
 sideGridL1 = efrs.spinObj(  0,   0, -90, sideGrid)
 sideGridL2 = efrs.shiftObj(-11, -16, 0, sideGridL1)
-sideGridR2 = efrs.shiftObj(83,   2,   0, sideGridL2)
+sideGridR2 = efrs.shiftObj(89,   2,   0, sideGridL2)
 
 cornerGridL1 = efrs.spinObj(  0,  0,  45, cornerGrid)
 cornerGridL2 = efrs.shiftObj(-11, -4.7, 0, cornerGridL1)
 
 cornerGridR1 = efrs.spinObj(  0,  0, -45, cornerGrid)
-cornerGridR2 = efrs.shiftObj(71, -3.2, 0, cornerGridR1)
+cornerGridR2 = efrs.shiftObj(74, -4.2, 0, cornerGridR1)
 
 scene  = backGrid + sideGridL2 + sideGridR2 
 scene +=          cornerGridL2 + cornerGridR2
