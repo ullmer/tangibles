469a344aedfc289703a5c9197ffbc002a8ef09bd
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-03 22:05:19 -0400

    progress

--- content/felsenreitenschule.01/synthFRS03.py
+++ content/felsenreitenschule.01/synthFRS03.py
@@ -7,14 +7,13 @@ from enoFRS import *
 
 efrs = enoFRS()
 
-backGrid  = efrs.synthPortal2DArray(10, 5)
-sideGrid  = efrs.synthPortal2DArray(5,  5)
+backGrid   = efrs.synthPortal2DArray(10, 5)
+sideGrid   = efrs.synthPortal2DArray( 5, 5)
+cornerGrid = efrs.synthPortal2DArray( 1, 5)
 
-sideGridL1 = efrs.spinObj(0, 0, -90, sideGrid)
-sideGridR1 = efrs.spinObj(0, 0,  90, sideGrid)
-
-sideGridL2 = efrs.shiftObj(0, -20, 0, sideGridL1)
-sideGridR2 = efrs.shiftObj(0,  20, 0, sideGridR1)
+sideGridL1 = efrs.spinObj(  0, 0, -90, sideGrid)
+sideGridL2 = efrs.shiftObj( -5, -13, 0, sideGridL1)
+sideGridR2 = efrs.shiftObj( 90,   0, 0, sideGridL2)
 
 scene = backGrid + sideGridL2 + sideGridR2
 
