4cbf5029f85cd32e099688bd9f6b1b9d6a76b8d5
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-05 22:00:27 -0400

    descriptive comments + steps toward YAML processing

--- content/felsenreitenschule.01/enoSolid.py
+++ content/felsenreitenschule.01/enoSolid.py
@@ -6,11 +6,22 @@ from solid import *  # load in SolidPython/SCAD support code; solid2?
 
 class enoSolid:
 
-  outGeom        = None
-  radialSegments = 25
+  outGeom        = None #output geometry representation
+  yamlFn         = None #YAML filename 
+  radialSegments = 25   #radial segments, used by OpenSCAD for curve transformation
+  geomDict       = None #geometries dictionary
+
+  ######## constructor / class initiation method ######## 
 
   def __init__(self, **kwargs): #all
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+
+    self.geomDict = {}
+
+    if self.yamlFn is not None:
+      self.loadYaml()
+
+  ######## constructor / class initiation method ######## 
 
   def shiftObj(self, dx, dy, dz, obj): return translate([dx, dy, dz])(obj) #convenience shift  func
   def spinObj(self,  ax, ay, az, obj): return rotate(   [ax, ay, az])(obj) #convenience rotate func
