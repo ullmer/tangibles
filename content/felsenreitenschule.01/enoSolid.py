91751cbe5c9b25defdd06159ad44d140a98fd8a1
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-07-05 22:10:16 -0400

    more yaml parsing functionality

--- content/felsenreitenschule.01/enoSolid.py
+++ content/felsenreitenschule.01/enoSolid.py
@@ -3,23 +3,30 @@
 # Written 2024-07-03
 
 from solid import *  # load in SolidPython/SCAD support code; solid2?
+import yaml, traceback
 
 class enoSolid:
 
   outGeom        = None #output geometry representation
   yamlFn         = None #YAML filename 
+  yamlD          = None #YAML data
+  yamlParams     = None
+  yamlGeomDescr  = None
+
   radialSegments = 25   #radial segments, used by OpenSCAD for curve transformation
   geomDict       = None #geometries dictionary
 
   ######## constructor / class initiation method ######## 
 
-  def __init__(self, **kwargs): #all
+  def __init__(self, **kwargs): 
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
 
     self.geomDict = {}
 
     if self.yamlFn is not None:
       self.loadYaml()
+
+  def err(self, msg): print("enoSolid error:", msg)
 
   ######## constructor / class initiation method ######## 
 
@@ -32,6 +39,26 @@ class enoSolid:
     else:               outGeom += obj
 
   def getGeom(self): return self.outGeom
+
+  ######## load yaml ######## 
+
+  def loadYaml(self):
+    if self.yamlFn is None: self.err("loadYaml: yamlFn == None");         return None
+
+    try:    yamlF = open(self.yamlFn, 'rt')
+    except: self.err("loadYaml: error opening yamlFn", self.yamlFn);      return None
+
+    try:    self.yamlD = yaml.safe_load(yamlF)
+    except: self.err("loadYaml safe_load error:"); traceback.print_exc(); return None
+
+    if 'params'    in self.yamlD: self.yamlParams    = self.yamlD['params']
+    if 'geomDescr' in self.yamlD: self.yamlGeomDescr = self.yamlD['geomDescr']
+
+    if self.yamlGeomDescr is not None: self.parseYamlGeomDescr()
+
+  ######## parsel yaml geometry description ######## 
+
+  def parseYamlGeomDescr(self): pass
 
   ############## render OpenSCAD output ##############
  
