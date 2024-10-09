f8d6ff94ee2c0dcdf1cd98f234622aca1f082c85
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-09 18:44:28 -0400

    progress

--- content/c-span.01/cspanMidi.py
+++ content/c-span.01/cspanMidi.py
@@ -6,6 +6,8 @@ import sys, os, yaml
 from pygame import time
 from enoMidiController import *
 
+############# cspan midi #############
+
 class cspanMidi:
 
   tagFn = 'cspan-tags.yaml'
@@ -20,6 +22,9 @@ class cspanMidi:
   def __init__(self, controllerName, **kwargs):
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
     if self.tagFn is not None: self.loadYaml()
+
+    self.initMidi()
+    self.midiIllumDefault()
 
   ############# load yaml #############
 
@@ -40,6 +45,12 @@ class cspanMidi:
     self.emc = enoMidiController(mcn, midiCtrlOutputId=mcoi, activateOutput=True)
     self.emc.registerControls(self.midiCB)
 
+  ############# midi illum default #############
+
+  def midiIllumDefault(self):
+
+    for i in range(64): emc.midiOut.note_on(i, i, 3)
+
   ############# midi cb #############
 
   def midiCB(control, arg): 
@@ -49,33 +60,7 @@ class cspanMidi:
 
     print("cspan midiCB %s: %s" % (tags[tagIdx], str(control)))
 
-for i in range(64): emc.midiOut.note_on(i, i, 3)
 
-print(tags[tagIdx])
-
-while True:
-  emc.pollMidi()
-  time.wait(100)
-
-### end ###
-#!/usr/bin/env python
-
-import sys, os
-from pygame import time
-
-import sys; sys.path.append("..")
-from enoMidiController import *
-
-#emc = enoMidiController('nu_mt3')
-#emc = enoMidiController('nu_dj2go2')
-#emc = enoMidiController('nov_launchpad_mk2')
-
-def midiCB(control, arg): print("midicb: ", str(control), str(arg))
-
-emc = enoMidiController('aka_apcmini2', midiCtrlOutputId=4, activateOutput=True)
-emc.registerControls(midiCB)
-
-for i in range(64): emc.midiOut.note_on(i, i, 3)
 
 while True:
   emc.pollMidi()
