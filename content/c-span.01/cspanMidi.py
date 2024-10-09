d69ae7c8f3c53ff66f35f251183e610f167751ec
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-09 18:42:15 -0400

    progress

--- content/c-span.01/cspanMidi.py
+++ content/c-span.01/cspanMidi.py
@@ -6,12 +6,22 @@ import sys, os, yaml
 from pygame import time
 from enoMidiController import *
 
-
 class cspanMidi:
 
   tagFn = 'cspan-tags.yaml'
   tagYd = None
   tags  = None
+
+  midiCtrlName     = 'aka_apcmini2
+  midiCtrlOutputId = 4
+
+  ############# constructor #############
+
+  def __init__(self, controllerName, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    if self.tagFn is not None: self.loadYaml()
+
+  ############# load yaml #############
 
   def loadYaml(self):
     self.tags  = []
@@ -21,6 +31,16 @@ class cspanMidi:
     ytags      = self.tagYd['tags']
     for tag in ytags: self.tags.append(tag)
 
+  ############# midi cb #############
+
+  def initMidi(self):
+    mcn  = self.midiCtrlName     
+    mcoi = self.midiCtrlOutputId 
+
+    self.emc = enoMidiController(mcn, midiCtrlOutputId=mcoi, activateOutput=True)
+    self.emc.registerControls(self.midiCB)
+
+  ############# midi cb #############
 
   def midiCB(control, arg): 
     global tags, tagIdx
@@ -28,9 +48,6 @@ class cspanMidi:
     if arg == 0: return #ignore pad release
 
     print("cspan midiCB %s: %s" % (tags[tagIdx], str(control)))
-
-emc = enoMidiController('aka_apcmini2', midiCtrlOutputId=4, activateOutput=True)
-emc.registerControls(midiCB)
 
 for i in range(64): emc.midiOut.note_on(i, i, 3)
 
