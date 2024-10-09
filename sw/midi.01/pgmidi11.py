b75a8805dbe14ee4e32e38c773e158989612aa79
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-09 17:34:12 -0400

    warming up to c-span migration

--- sw/midi.01/pgmidi11.py
+++ sw/midi.01/pgmidi11.py
@@ -9,22 +9,10 @@ from time import sleep
 #emc = enoMidiController('nu_dj2go2')
 #emc = enoMidiController('nov_launchpad_mk2')
 
+def midiCB(control, arg): print("midicb: ", str(control), str(arg))
+
 emc = enoMidiController('aka_apcmini2', midiCtrlOutputId=4, activateOutput=True)
-emc.registerControls(emc.debugCallback)
-
-#emc.clearLights()
-#emc.rightMarginRainbow()
-
-#try:
-#  for i in range(8):
-#    for j in range(8):
-#      emc.setLaunchpadXYcolor(i, j, 5*i, 0, 5*j)
-#except: pass
-
-#pygame.midi.init()
-#i = pygame.midi.Input(1)
-#o = pygame.midi.Output(4)
-#for i in range(64): o.note_on(i, i, 3)
+emc.registerControls(midiCB)
 
 for i in range(64): emc.midiOut.note_on(i, i, 3)
 
