feb34503b0c4d28a6f78ea6ed7a5d86cfa8cb123
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-12-29 14:42:48 -0500

    something very strange had happened in there, hope it is resolved

--- content/cuSocClasses.01/coursesCsv.py
+++ content/cuSocClasses.01/coursesCsv.py
@@ -163,123 +163,12 @@ class Courses: #not catching any errors; caveat emptor
 
   ################## get course by idx ##################
 
-
-    try:
-      f        = open(self.fnMain, 'rt')  
-      rdr      = csv.reader(f, delimiter=',', quotechar='"')
-      firstRow = True
-
-      ipf1, ipf2 = self.instrPostfix1, self.instrPostfix2 
-
-      for row in rdr:   ### Process main file
-        if firstRow: self.processCsvHeaderMain(row); firstRow = False; continue
-        c = Course()
-        c.setFields(self.csvHeaderFields, row)
-
-        crse = c.getField('Crse')
-        if crse.find('4+6') > -1: crse2 = 'x' + crse[3:]; c.setField('Crse', crse2)
-        courseId = c.getCourseId()
-
-        instr1 = c.getField('Instructor')
-        if   instr1.find(ipf1) > -1: instr2 = instr1[:-4]; c.setField('Instructor', instr2)
-        elif instr1.find(ipf2) > -1: instr2 = instr1[:-6]; c.setField('Instructor', instr2)
-
-        instr1 = c.getField('InstructorTrunc')
-        if   instr1.find(ipf1) > -1: instr2 = instr1[:-4]; c.setField('InstructorTrunc', instr2)
-        elif instr1.find(ipf2) > -1: instr2 = instr1[:-6]; c.setField('InstructorTrunc', instr2)
-
-        self.coursesDict[courseId] = c
-
-        subj = c.getField('Subj')
-        if subj not in self.courseIdsByPrefix: self.courseIdsByPrefix[subj] = []
-        self.courseIdsByPrefix[subj].append(courseId)
-
-      f.close()
-
-      f        = open(self.fnAbbrev, 'rt') # Read abbrev file next
-      rdr      = csv.reader(f, delimiter=',', quotechar='"')
-      firstRow = True
-
-      for row in rdr:   ### Process abbreviations file
-        if firstRow: firstRow = False; continue
-   
-        try:
-          if len(row) == 3: subject, course, abbrev = row
-          else: self.msg("loadCsv problem with abbrev file re input: " + str(row)); continue
-
-          courseID = subject + course
-          c = self.getCourseById(courseID)
-          if c is None: self.msg("loadCsv: problem in assigning abbrev to " + courseId)
-          else: 
-            c.setField('abbrevTitle', abbrev)
-            a,c,g,h,i = row[3:] # applications,core,graphics,hardware,interaction #hacky, but time-pressed
-
-            c.cats = self.cats
-            c.setFields(c.cats, [a,c,g,h,i])
-
-            for cat in cats:
-              if c.getField(cat) in self.catAssocChars: self.catsDict[cat].append(courseID)
-
-        except: self.err("loadCsv abbrev issue")
-    except: self.err("loadCsv issue")
-
-  ################## process CSV header ##################
-
-  def processCsvHeaderMain(self, row): 
-    self.csvHeaderFields     = row 
-    self.csvHeaderFieldsDict = {}
-    idx = 0
-   
-    for field in row:
-      self.csvHeaderFieldsDict[field] = idx
-      idx += 1
-
-  ################## print course IDs ##################
-
-  def printCourseIds(self): 
-    try:
-      nc = self.getNumCourses()
-      for i in range(nc):
-        c = self.getCourseByIdx(i); c.printCourseId()
-    except: self.err("printCourseIds issue")
-
-  ################## get number of courses ##################
-
-  def getNumCourses(self): 
-    try:
-      cidList = list(self.coursesDict.keys())
-      return len(cidList)
-    except: self.err("getNumCourses issue:")
-
-  ################## get course by idx ##################
-
-  def getCourseById(self, courseId): 
-    try:
-      if courseId in self.coursesDict: return self.coursesDict[courseId]
-      return None
-      
-    except: self.err("getCourseById issue: " + courseId); return
-
-  ################## get course by idx ##################
-
   def getCourseIdByIdx(self, i): 
     try:
       coursesL = list(self.coursesDict)
       result = coursesL[i]
       return result
     except: self.err("getCourseIdByIdx" + str(i)); return -1
-
-  ################## get course by idx ##################
-
-  def getCourseByIdx(self, i): 
-    try:
-      cid    = self.getCourseIdByIdx(i)
-      result = self.coursesDict[cid]
-
-      if self.verbose: msgStr = "getCourseByIdx %i: %s" % (i, str(result)); print(msgStr) 
-      return result
-      
-    except: self.err("getCourseByIdx issue: " + str(i)); return -1
 
   ################## get course ids by prefix##################
 
