731b49fb4bb559eb72f530a485c966544b18617f
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-12-29 19:15:38 -0500

    progress

--- content/cuSocClasses.01/coursesCats.py
+++ content/cuSocClasses.01/coursesCats.py
@@ -23,10 +23,28 @@ class CoursesCats(Courses):
   def err(self, msg): print("CoursesPgzCats error:", msg); traceback.print_exc()
   def msg(self, msg): print("CoursesPgzCats msg:",   msg)
 
+  ################## get ##################
+
+  def getCats(self): return self.cats
+
+  def getNumCoursesInCat(self, cat): 
+    if cat not in self.cats: self.msg("getNumCoursesInCats: cat not found: " + str(cat)); return None
+    result = len(self.cats[cat])
+    return result
+
+  def getCoursesInCat(self, cat): 
+    if cat not in self.cats: self.msg("getCoursesInCats: cat not found: " + str(cat)); return None
+    result = self.cats[cat]
+    return result
+
 ################## main ################## 
 
 if __name__ == "__main__":
   cc = CoursesCats()
+  cats = cc.getCats()
+  for cat in cats:
+    numCats = cc.getNumCoursesInCat(cat)
+    print("%s: %i courses" % (cat, numCats))
 
 #def draw(): screen.clear(); cpgza.draw(screen)
 #def on_mouse_down(pos):     pass #cpgz.on_mouse_down(pos)
