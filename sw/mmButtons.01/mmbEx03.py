2fb338980beb18469989a35c08f5ed33daa5f7b0
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-11 14:15:37 -0400

    progress

--- sw/mmButtons.01/mmbEx03.py
+++ sw/mmButtons.01/mmbEx03.py
@@ -9,6 +9,7 @@ from tkinter           import *
 
 root = Tk()    # Create the root (base) window 
 etba = enoTkiTicButtonArray(parent=root, yamlFn='mm03.yaml')
+#etba = enoTkiTicButtonArray(parent=root, yamlFn='mm03.yaml', ticHwPresent=True)
 
 root.mainloop() # Start the event loop
 
