e55c085c4e454082149a7ac59ed4d568e842a3db
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-11 14:05:22 -0400

    progress

---
+++ sw/mmButtons.01/mmbEx03.py
@@ -0,0 +1,15 @@
+#
+# Brygg Ullmer, Sarah Sears, and Uzayr Syed
+# Begun 2025-04-11
+
+# https://en.wikipedia.org/wiki/Tkinter
+
+from enoTkiTicButtonArray import *
+from tkinter           import *
+
+root = Tk()    # Create the root (base) window 
+etba = enoTkiButtonArrayVariant(parent=root, yamlFn='mm03.yaml')
+
+root.mainloop() # Start the event loop
+
+### end ###
