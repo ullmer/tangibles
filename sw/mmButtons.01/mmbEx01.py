752627c37e8bc06fff28c99d16f0302f3fc53ddd
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-11 11:40:57 -0400

    progress

--- sw/mmButtons.01/mmbEx01.py
+++ sw/mmButtons.01/mmbEx01.py
@@ -7,6 +7,8 @@
 from mmButtons import *
 from tkinter   import *
 
+def dButtonCb(arg): print("d button callback, argument " + str(arg))
+
 root = Tk()    # Create the root (base) window 
 etba = enoTkiButtonArray(parent=root)
 root.mainloop() # Start the event loop
