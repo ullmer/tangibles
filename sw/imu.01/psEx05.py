33925b04a4ab736c3a98f39fa5d636b249ad8c51
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-12 17:51:37 -0400

    progress

--- sw/imu.01/psEx05.py
+++ sw/imu.01/psEx05.py
@@ -4,14 +4,18 @@
 
 # https://en.wikipedia.org/wiki/Tkinter
 
-from enoTkiTicButtonArray import *
+from enoTkiButtonArray import *
 from enoEmbSerialConsole  import *
 from tkinter              import *
 
+class enoTkiEmbButtonArray(enoTkiButtonArray, enoEmbSerialConsole): #inherits methods from both
+  def __init__(self, **kwargs):  #this and subsequent two lines each include ~"opaque magic"
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    super().__init__()           #call the constructors for both enoTkiButtonArray and enoEmbSerialConsole
+
 root = Tk()    # Create the root (base) window 
 
-etba = enoTkiTicButtonArray(parent=root, yamlFn='mm03.yaml')
-eesc = enoEmbSerialConsole()
+etba = enoTkiEmbButtonArray(parent=root, yamlFn='ps05.yaml')
 
 root.mainloop() # Start the event loop
 
