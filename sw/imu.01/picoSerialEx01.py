ca6362d519ae2ca13736e74ad6e6501679105981
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-12 12:31:59 -0400

    rename

--- sw/imu.01/picoSerialEx01.py
+++ sw/imu.01/picoSerialEx01.py
@@ -1,7 +1,7 @@
 #Code ~by CoPilot, 2025-04-12
 # pip install pyserial
 
-import serial
+import serial, time
 import serial.tools.list_ports
 
 def find_pico_port():
@@ -21,13 +21,23 @@ def main():
   print(f"Raspberry Pi Pico found on port {pico_port}")
 
   # Establish serial communication
-  ser = serial.Serial(pico_port, 115200)  # Adjust baud rate if necessary
+  ser = serial.Serial(pico_port, 115200, timeout=1)  # Adjust baud rate if necessary
+
+  cpHeader = 'Adafruit CircuitPython'
+  cpHLen   = len(cpHeader)
 
   try:
+    ser.write(b'\n');         ser.flush()
     while True:
       if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').rstrip()
         print(f"Received: {line}")
+
+        if line[0:cpHLen] == cpHeader: 
+          print("!!!")
+          time.sleep(0.1)
+          ser.write("print(3)\n\n".encode('utf-8')); ser.flush()
+
         # You can send data to Pico using ser.write() method
         # ser.write(b'Your message here\n')
   except KeyboardInterrupt:
