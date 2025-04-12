2bf80ede8ff9e71c1b77cd5f04ddb1d436b4121b
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-12 14:53:20 -0400

    progress

--- sw/imu.01/picoSerialEx03.py
+++ sw/imu.01/picoSerialEx03.py
@@ -5,7 +5,9 @@
 import serial, time
 import serial.tools.list_ports
 
-def find_pico_port():
+################## find embedded-device serial port ##################
+
+def find_embedded_port():
   ports = serial.tools.list_ports.comports()
   for port in ports:
     print("port description: " + str(port.description))
@@ -13,11 +15,15 @@ def find_pico_port():
       return port.device
   return None
 
+################## send interrupt-and-clear to circuitpython device ##################
+
 def interruptAndClear(ser):
   ser.write(b'\x03'); time.sleep(0.1) # Send Ctrl+C to interrupt any running code
   ser.write(b'\x01'); time.sleep(0.1) # Send Ctrl+A to enter raw REPL mode
 
-def send_command(ser, command):
+################## send command to circuitpython device ##################
+
+def sendCommand(ser, command):
   interruptAndClear(ser)
   ser.write((command + '\n').encode('utf-8')); ser.flush(); time.sleep(0.1) 
   ser.write(b'\x04'); ser.flush(); time.sleep(0.1) # Send Ctrl+D to execute the command
@@ -26,26 +32,29 @@ def send_command(ser, command):
   response2 = response[29:-4] #clear prefix (some interrupt&clear-based) and postfix
   return response2
 
+################## command-line interface to serial-linked embedded device ##################
+
 def cli(ser):
   try:
     while True:
       command = input("Enter command for Pico: ")
       if command.lower() == "exit": break
-      response = send_command(ser, command)
-      print(f"Response: {response}")
+      response = sendCommand(ser, command)
+      print(f">> {response}")
+
   except KeyboardInterrupt:
     print("Exiting...")
 
+################## main ##################
+
 def main():
-  pico_port = find_pico_port()
+  pico_port = find_embedded_port()
   if pico_port is None:
-    print("Raspberry Pi Pico not found.")
-    return
+    print("CircuitPython device not found."); return
 
-  print(f"Raspberry Pi Pico found on port {pico_port}")
+  print(f"CircuitPython device found on port {pico_port}")
 
-  # Establish serial communication
-  ser = serial.Serial(pico_port, 115200, timeout=1)  # Adjust baud rate if necessary
+  ser = serial.Serial(pico_port, 115200, timeout=1); # Establish serial communication
 
   cpHeader = 'Adafruit CircuitPython'
   cpHLen   = len(cpHeader)
@@ -59,18 +68,17 @@ def main():
         interruptAndClear(ser)
 
         if line[0:cpHLen] == cpHeader: 
-          print("!!!")
+          print(connection established"
           time.sleep(0.1)
-          ser.write("print(3)\n\n".encode('utf-8')); ser.flush()
+          #ser.write("print(3)\n\n".encode('utf-8')); ser.flush()
           cli(ser)
 
-        # You can send data to Pico using ser.write() method
-        # ser.write(b'Your message here\n')
   except KeyboardInterrupt:
-    print("Exiting...")
+    print("exiting...")
   finally:
     ser.close()
 
 if __name__ == "__main__":
   main()
 
+### end ###
