2109c8bc5e40578bd438ccd6acc5eb2573b2501b
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-12 15:30:52 -0400

    experiment to interact with remote serial control

---
+++ sw/imu.01/enoEmbBase.py
@@ -0,0 +1,17 @@
+# Enodia embedded base functions 
+# Brygg Ullmer, Clemson University
+# Begun 2025-04-12
+
+import time
+import board
+import busio
+import supervisor
+import alarm
+
+from enoEmbedJserver import *
+from enoIMUjserver   import *
+
+ejs = enoEmbedJserver()
+ijs = enoIMUjserver()
+
+### end ###
