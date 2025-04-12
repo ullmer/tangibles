0aa42544a8410c6b51ae8b5dac17659f758ce97a
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-11 21:47:17 -0400

    progress

--- sw/imu.01/enoIMUjlistener.py
+++ sw/imu.01/enoIMUjlistener.py
@@ -3,6 +3,7 @@
 # Begun 2025-04-11 
 
 import sys
+import json
 
 #https://learn.adafruit.com/adafruit-lsm6ds3tr-c-6-dof-accel-gyro-imu/python-circuitpython
 #https://github.com/xioTechnologies/Fusion/blob/main/Python/simple_example.py
@@ -10,16 +11,36 @@ import sys
 try:    import imufusion
 except: print("imufusion class not found!"); sys.exit(-1)
 
+
 class enoIMUjlistener: #enodia IMU JSON ~listener
 
   #accelGyroJsonFmt = "{s:ag1, a:[%2.2f,%2.2f,%2.2f], g:[%2.2f,%2.2f,%2.2f]}" 
-
   lastUpdateInTicks = -1
+  currentPos        = None
+  currentOrient     = None
+  ahrs              = None
 
-  def __init__(self): 
+  ######### constructor #########
 
-  def parseAccelGyroJson(self, accelGyroAxisVals):
-    result = self.accelGyroJsonFmt % accelGyroAxisVals
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    self.initImuFsn()
+
+  def initImuFsn(self): self.ahrs = imufusion.Ahrs()
+
+  def msg(self, msgStr): print("enoIMUjlistener: " + str(msgStr)) #toward future redirectability
+
+  ######### parse accelerometer/gyro json #########
+
+  def parseAccelGyroJson(self, agStr):
+    try:
+      parsed_data = json.loads(agStr)
+
+      self.evalAccelGyroJson(self, agDict)
+
+      if 's' not in parsed_data: sle
+
+
     return result
 
 ### end ###
