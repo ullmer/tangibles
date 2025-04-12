8418e26346f570a4f5eb794665ee1b067f5ab359
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-12 18:14:03 -0400

    progress

---
+++ sw/imu.01/psEx04.py
@@ -0,0 +1,11 @@
+#
+# Brygg Ullmer, Sarah Sears, and Uzayr Syed
+# Begun 2025-04-11
+
+# https://en.wikipedia.org/wiki/Tkinter
+
+from enoEmbSerialConsole  import *
+
+eesc = enoEmbSerialConsole(autolaunchCli=True)
+
+### end ###
