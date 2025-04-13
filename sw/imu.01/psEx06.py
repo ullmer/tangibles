3baac3a2771d777493b9b448902340d6f43d9399
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-13 00:28:35 -0400

    progress

---
+++ sw/imu.01/psEx06.py
@@ -0,0 +1,29 @@
+#
+# Brygg Ullmer, Sarah Sears, and Uzayr Syed
+# Begun 2025-04-11
+
+# https://en.wikipedia.org/wiki/Tkinter
+
+from enoTkiButtonArray import *
+from enoEmbSerialConsole  import *
+from tkinter              import *
+import time
+
+class enoTkiEmbButtonArray(enoTkiButtonArray, enoEmbSerialConsole): #inherits methods from both
+  def __init__(self, **kwargs):  
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    enoTkiButtonArray.__init__(self)        
+    enoEmbSerialConsole.__init__(self)     
+
+  def readIMUmulti(self):
+    self.sendCmd("ejs.readIMU(50)")
+    while True: 
+      self.readSerial()
+      time.sleep(.05)
+
+root = Tk()    # Create the root (base) window 
+etba = enoTkiEmbButtonArray(parent=root, yamlFn='ps05.yaml')
+
+root.mainloop() # Start the event loop
+
+### end ###
