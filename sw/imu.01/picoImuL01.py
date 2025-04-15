65a638c55b8d181ca5a1463a47fb7a9340bb9a75
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-15 08:39:02 -0400

    low-level hello world

---
+++ sw/imu.01/picoImuL01.py
@@ -0,0 +1,12 @@
+from adafruit_lsm6ds.lsm6ds3 import LSM6DS3 as LSM6DS
+import time
+import board
+import busio
+
+i2c = busio.I2C(board.GP7, board.GP6)
+accel_gyro = LSM6DS(i2c, address=0x6b)
+
+a = accel_gyro.acceleration
+g = accel_gyro.gyro
+
+agJson = "{s:ag1, a:[%2.2f,%2.2f,%2.2f], g:[%2.2f,%2.2f,%2.2f]}" % (a+g)
