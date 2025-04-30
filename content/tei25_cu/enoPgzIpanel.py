c4e50f51e408845b4f32218b92cfca77ded3a3da
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-04-30 09:36:22 -0400

    progress

---
+++ content/tei25_cu/enoPgzIpanel.py
@@ -0,0 +1,36 @@
+# Interaction panel MIDI code 
+# Brygg Ullmer, Clemson University
+# Begun 2024-10-09
+
+import sys, os, yaml, traceback
+from pygame import time
+from enoMidiController import *
+from enoMidiAkai       import *
+from enoIpanelYaml     import *
+
+############# enodia interaction panel midi #############
+
+class enoPgzIpanel(enoIpanelMidi):
+
+  ############# constructor #############
+
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    super().__init__()
+
+  ############# error, msg #############
+ 
+  def err(self, msgStr): print("enoPgzIpanel error: " + str(msgStr)); traceback.print_exc(); 
+  def msg(self, msgStr): print("enoPgzIpanel msg: "   + str(msgStr))
+
+  ############# screen augmentation of selected grid locus #############
+
+  def screenAugmentSelectedGrid(self, coordTuple):
+    try:
+      i, j = coordTuple
+      gridVal = self.getMatrixLocus(i, j)
+      self.msg("screenAugmentSelectedGrid called, " + \
+        str(coordTuple) + " " + str(gridVal))
+    except: self.err("screenAugmentSelectedGrid"); return None
+
+### end ###
