add40be59835e4e8731e1a52fba68d5a6b41b07b
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-05-09 16:40:28 -0400

    class-descendant evolution

---
+++ content/us_nps.01/exploratory/pgzScale02.py
@@ -0,0 +1,38 @@
+# Hacky interim approach to allow animatable Actor scaling in pygame zero
+# Brygg Ullmer, Clemson University
+# Begun 2025-05-09
+
+import pygame, traceback
+
+WIDTH, HEIGHT = 1920, 1080
+
+def ActorScaled(Actor): #scaled actor
+  scale          = 1.
+  lastScaleVal   = None
+  lastScaledSurf = None
+  scaleIncrement = 1000
+
+  def update(self): #updated scaled surface
+    try:
+      scaleInt     = int(self.scale * self.scaleIncrement)  #toward nuancing precision issues 
+      lastScaleInt = int(self.lastScaleVal * self.scaleIncrement)
+
+      if lastScaleVal != None or scaleInt == lastScaleInst: return  #nothing to do
+      w, h           = self.width * self.scale, self.height * self.scale
+      lastScaledSurf = pygame.transform.scale(self._orig_surf, (w, h))
+
+    except: print("ActorScaled update issue"); traceback.print_exc(); return None
+
+  def draw(self): 
+    if self.scale == 1.: super().draw()
+    else: 
+      self.update()
+      self.blit(self.lastScaledSurf, self.pos)
+
+a1 = ActorScaled("ipan_usa_bea08c")
+animate(a1, scale=1, duration=2.)
+
+def draw(): 
+  a1.draw()
+
+### end ###
