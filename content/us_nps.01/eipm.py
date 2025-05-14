c6ccbf97fea6e93282e9b7247c17b98331bee285
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-05-14 09:32:07 -0400

    moving run code to separate file

---
+++ content/us_nps.01/eipm.py
@@ -0,0 +1,29 @@
+# Interaction panel MIDI manager
+# Brygg Ullmer, Clemson University
+# Begun 2025-04-28
+
+from enoIpanelPgzMgr import *
+
+WIDTH, HEIGHT = 2100, 1150
+
+############# main #############
+
+epim = enoIpanelPgzMgr()
+
+print("=" * 70)
+
+epi1 = enoIpanelPgz(tagFn = 'yaml/us-bea.yaml',     casePaired=True,  autolaunchMidi=False)
+epi2 = enoIpanelPgz(tagFn = 'yaml/cspan-tags.yaml', casePaired=False, autolaunchMidi=False)
+
+epim.registerIpanel(epi1, 0) #bootstrapping logic, to be reworked
+epim.registerIpanel(epi2, 1)
+  
+def draw():   
+  screen.clear(); 
+  epim.draw(screen)
+
+def update(): 
+  epim.pollMidi()
+  if epim.panelUpdated(): epim.restageActors() #probably to be renamed
+
+### end ###
--- content/us_nps.01/enoIpanelPgzMgr.py
+++ content/us_nps.01/enoIpanelPgzMgr.py
@@ -175,22 +175,22 @@ class enoIpanelPgzMgr(enoIpanelMidiMgr):
 
 ############# main #############
 
-epim = enoIpanelPgzMgr()
-
-print("=" * 70)
-
-epi1 = enoIpanelPgz(tagFn = 'yaml/us-bea.yaml',     casePaired=True,  autolaunchMidi=False)
-epi2 = enoIpanelPgz(tagFn = 'yaml/cspan-tags.yaml', casePaired=False, autolaunchMidi=False)
-
-epim.registerIpanel(epi1, 0) #bootstrapping logic, to be reworked
-epim.registerIpanel(epi2, 1)
-  
-def draw():   
-  screen.clear(); 
-  epim.draw(screen)
-
-def update(): 
-  epim.pollMidi()
-  if epim.panelUpdated(): epim.restageActors() #probably to be renamed
-
+#epim = enoIpanelPgzMgr()
+#
+#print("=" * 70)
+#
+#epi1 = enoIpanelPgz(tagFn = 'yaml/us-bea.yaml',     casePaired=True,  autolaunchMidi=False)
+#epi2 = enoIpanelPgz(tagFn = 'yaml/cspan-tags.yaml', casePaired=False, autolaunchMidi=False)
+#
+#epim.registerIpanel(epi1, 0) #bootstrapping logic, to be reworked
+#epim.registerIpanel(epi2, 1)
+#  
+#def draw():   
+#  screen.clear(); 
+#  epim.draw(screen)
+#
+#def update(): 
+#  epim.pollMidi()
+#  if epim.panelUpdated(): epim.restageActors() #probably to be renamed
+#
 ### end ###
