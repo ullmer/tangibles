3144b5eb6a720548d1dbfba0041a68074a783fc0
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-06-08 13:53:34 -0400

    stub

---
+++ sw/mediaBlocks.2025/visualization/pivy_visualization.py
@@ -0,0 +1,10 @@
+# Placeholder for Pivy-based 3D visualization module
+class PivyVisualization:
+    def __init__(self, config):
+        self.config = config
+
+    def setup(self):
+        pass
+
+    def render(self):
+        pass
