3e68ec550c73151ddc603d5daf551592feab5b5d
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-16 23:39:57 -0400

    decimal variations

--- content/tei26/yaml/physDescrReeds.pl
+++ content/tei26/yaml/physDescrReeds.pl
@@ -14,17 +14,21 @@ reed_source(
 % Reed width options
 reed_width("11/64", 0.1718).
 reed_width("3/16",  0.1875).
-reed_width("1/4",   0.25).
-reed_width("3/8",   0.375).
-reed_width("1/2",   0.5).
-reed_width("5/8",   0.625).
-reed_width("3/4",   0.75).
-reed_width("7/8",   0.875).
-reed_width("1",     1.0).
+reed_width("1/4",   0.2500).
+reed_width("3/8",   0.3750).
+reed_width("1/2",   0.5000).
+reed_width("5/8",   0.6250).
+reed_width("3/4",   0.7500).
+reed_width("7/8",   0.8750).
+reed_width("1",     1.0000).
 
 % Fabrication constraints
-reed_cut_constraint( range("0",   "1/4"), min_cut("1/16"), preferred_cut("3/32")).
-reed_cut_constraint( range("3/8", "1/2"), min_cut("3/32"), preferred_cut("1/8")).
-reed_cut_constraint( range("5/8", "1"),   min_cut("1/8"),  preferred_cut("5/32")).
+reed_cut_constraint_frac( range("0",   "1/4"), min_cut("1/16"), preferred_cut("3/32")).
+reed_cut_constraint_frac( range("3/8", "1/2"), min_cut("3/32"), preferred_cut("1/8")).
+reed_cut_constraint_frac( range("5/8", "1"),   min_cut("1/8"),  preferred_cut("5/32")).
+
+reed_cut_constraint_dec( range(0,     .25), min_cut(.0625), preferred_cut(.0938)).
+reed_cut_constraint_dec( range(.375,  .50), min_cut(.0938), preferred_cut(.1250)).
+reed_cut_constraint_dec( range(.675, 1.00), min_cut(.1250), preferred_cut(.1560)).
 
 %%% end %%%
