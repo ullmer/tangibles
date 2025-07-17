03a99417864d969f64712400cdae3f5f9c778e52
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-07-16 23:13:43 -0400

    progress

---
+++ content/tei26/yaml/exPdr01.pl
@@ -0,0 +1,10 @@
+use_module(library(yaml)).
+
+yaml_read('physDescrReeds.yaml', Dict),
+   get_dict(reed, Dict, Reed),
+   get_dict(fab, Reed, Fab),
+   get_dict(constraints, Fab, Constraints),
+   get_dict(copilotEstimates, Constraints, Estimates),
+   member(C, Estimates),
+   writeln(C).
+
