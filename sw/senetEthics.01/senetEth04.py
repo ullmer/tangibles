734264b2ee68efafbbde0553abaa8a40a235ae5d
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-25 13:11:12 -0400

    progress

---
+++ sw/senetEthics.01/senetEth04.py
@@ -0,0 +1,39 @@
+# Senet ethics example
+# Brygg Ullmer, Clemson University
+# Begun 2026-03-24
+
+WIDTH, HEIGHT=1535,1180
+
+a  = Actor('senet03k')
+
+peopleAbbrev = ['gel', 'jgh', 'tr', 'fdr', 'er', 'rf']
+people = []; x, y = 300, 500; dx = 100
+for pa in peopleAbbrev: 
+  p = Actor(pa); people.append(p)
+  p.pos = (x,y); x+= dx
+
+def draw(): 
+  a.draw(); 
+  for p in people: p.draw()
+
+selectedActor = None
+
+def on_mouse_down(pos): 
+  global selectedActor
+  for p in people:
+    if p.collidepoint(pos): 
+      selectedActor = p
+
+def on_mouse_up(): 
+  global selectedActor
+  selectedActor = None
+
+
+def on_mouse_move(self, rel):
+  if selectedActors is not None: 
+    dx, dy = rel
+    x,  y  = selectedActor.pos
+    x += dx; y += dy
+    selectedActor = (x,y)
+
+### end ###
