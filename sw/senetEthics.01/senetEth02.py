ca7f01c970ab2e08e4bc17eb15acda51685c7a97
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-25 13:05:44 -0400

    progress

--- sw/senetEthics.01/senetEth02.py
+++ sw/senetEthics.01/senetEth02.py
@@ -4,8 +4,10 @@
 
 WIDTH, HEIGHT=1535,1180
 
-a = Actor('senet03k')
+a  = Actor('senet03k')
+p1 = Actor('gel')
+p1.pos=(500,500)
 
-def draw(): a.draw()
+def draw(): a.draw(); p1.draw()
 
 ### end ###
---
+++ sw/senetEthics.01/senetEth03.py
@@ -0,0 +1,19 @@
+# Senet ethics example
+# Brygg Ullmer, Clemson University
+# Begun 2026-03-24
+
+WIDTH, HEIGHT=1535,1180
+
+a  = Actor('senet03k')
+
+peopleAbbrev = ['gel', 'jgh', 'tr', 'fdr', 'er', 'rf']
+people = []; x, y = 300, 500; dx = 100
+for pa in peopleAbbrev: 
+  p = Actor(pa); people.append(p)
+  p.pos = (x,y); x+= dx
+
+def draw(): 
+  a.draw(); 
+  for p in people: p.draw()
+
+### end ###
