c46d0da6a66f1e53f3445e8aabb87b7cfbf920b5
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-25 00:13:52 -0400

    progress

--- sw/senetEthics.01/enoSegmentedImgCoords.py
+++ sw/senetEthics.01/enoSegmentedImgCoords.py
@@ -3,35 +3,61 @@
 # Begun 2026-03-24
 
 import numpy as np
+from ataBase import *
 
-def lerp(a, b, t): return a * (1 - t) + b * t
+class EnoSegmentedImgCoords(AtaBase):
 
-def imgGrid2coord(x, y, data):
-  # Extract components
-  TL, TR, BL, BR = map(lambda p: np.array(p, dtype=float), data["coords"])
-  tTop    = np.array(data["sectorIndices"]["top"])
-  tBottom = np.array(data["sectorIndices"]["bottom"])
-  tLeft   = np.array(data["sectorIndices"]["left"])
-  tRight  = np.array(data["sectorIndices"]["right"])
+  yamlFn=None
+  yamlD = None
 
-  # Mid-fractions for the cell's center
-  tTopMid    = 0.5*(tTop[x]  + tTop[x+1])
-  tBottomMid = 0.5*(tBottom[x] + tBottom[x+1])
-  tLeftMid   = 0.5*(tLeft[y]   + tLeft[y+1])
-  tRightMid  = 0.5*(tRight[y]  + tRight[y+1])
+  ############# constructor #############
+  def __init__(self, **kwargs): 
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    if self.yamlFn is not None: self.loadYaml()
 
-  # Convert to unified horizontal/vertical fractions
-  tx = 0.5*(tTopMid + tBottomMid)
-  ty = 0.5*(tLeftMid + tRightMid)
+  ############# lerp #############
 
-  # Bilinear interpolation of a quadrilateral
-  center = (
-    TL*(1-tx)*(1-ty) +
-    TR*(tx)*(1-ty) +
-    BL*(1-tx)*ty +
-    BR*(tx)*ty
-  )
+  def loadYaml(self):
+    try:
+      if self.yamlFn is None: self.msg("yamlFn is not initiated"); return None
+     
+    except: self.err("loadYaml")
 
-  return tuple(center)
+  ############# lerp #############
+
+  def lerp(self, a, b, t): return a * (1 - t) + b * t
+
+  ############# image grid to coordinate #############
+  
+  def imgGrid2Coord(self, x, y):
+    try:
+      data = self.yamlD
+      # Extract components
+      TL, TR, BL, BR = map(lambda p: np.array(p, dtype=float), data["coords"])
+      tTop    = np.array(data["sectorIndices"]["top"])
+      tBottom = np.array(data["sectorIndices"]["bottom"])
+      tLeft   = np.array(data["sectorIndices"]["left"])
+      tRight  = np.array(data["sectorIndices"]["right"])
+    
+      # Mid-fractions for the cell's center
+      tTopMid    = 0.5*(tTop[x]  + tTop[x+1])
+      tBottomMid = 0.5*(tBottom[x] + tBottom[x+1])
+      tLeftMid   = 0.5*(tLeft[y]   + tLeft[y+1])
+      tRightMid  = 0.5*(tRight[y]  + tRight[y+1])
+    
+      # Convert to unified horizontal/vertical fractions
+      tx = 0.5*(tTopMid + tBottomMid)
+      ty = 0.5*(tLeftMid + tRightMid)
+    
+      # Bilinear interpolation of a quadrilateral
+      center = (
+        TL*(1-tx)*(1-ty) +
+        TR*(tx)*(1-ty) +
+        BL*(1-tx)*ty +
+        BR*(tx)*ty
+      )
+    
+      return tuple(center)
+    except: self.err("imgGrid2Coord");
 
 ### end ###
