2cc49ed97928b23bb6712edc22e70cfdb9e69546
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2026-03-25 14:55:19 -0400

    illustrating beginning of abstraction

---
+++ sw/senetEthics.01/senetEth06.py
@@ -0,0 +1,18 @@
+# Senet ethics example
+# Brygg Ullmer, Clemson University
+# Begun 2026-03-24
+
+WIDTH, HEIGHT=1535,1180
+
+from senetEth06People import *
+
+a  = Actor('senet03k')
+se6p = senetEth06People()
+
+def draw(): a.draw(); se6p.draw()
+
+def on_mouse_down(pos): se6p.on_mouse_down(pos)
+def on_mouse_up():      se6p.on_mouse_up()
+def on_mouse_move(rel): se6p.on_mouse_move(rel)
+
+### end ###
---
+++ sw/senetEthics.01/senetEth06People.py
@@ -0,0 +1,42 @@
+# Senet ethics example -- first example of classes
+# Brygg Ullmer, Clemson University
+# Begun 2026-03-25
+
+
+class senetEth06People:
+
+  peopleAbbrev = ['gel', 'jgh', 'tr', 'fdr', 'er', 'rf']
+  people = None
+  basePos = (300, 500)
+  dx = 100
+  selectedActor = None
+
+  def __init__(self): self.buildActors()
+
+  def buildActors(self):
+    self.people = []
+    x, y = self.basePos
+    for pa in self.peopleAbbrev:
+      p = Actor(pa); self.people.append(p)
+      p.pos = (x, y); x += self.dx 
+
+  def draw(self): 
+    for p in self.people: p.draw()
+
+  def on_mouse_down(self, pos): 
+    for p in self.people:
+      if p.collidepoint(pos): 
+        self.selectedActor = p
+
+  def on_mouse_up(self): 
+    animate(self.selectedActor, pos=(100,100), tween='accel_decel')
+    self.selectedActor = None
+
+  def on_mouse_move(self, rel):
+    if self.selectedActor is not None: 
+      dx, dy = rel
+      x,  y  = self.selectedActor.pos
+      x += dx; y += dy
+      self.selectedActor.pos = (x,y)
+
+### end ###
