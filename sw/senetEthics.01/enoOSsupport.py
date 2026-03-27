8ccf1da8b05326f868a86ea69a9b6962d05c6ea6
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-27 16:13:17 -0400

    progress

--- sw/senetEthics.01/enoOSsupport.py
+++ sw/senetEthics.01/enoOSsupport.py
@@ -4,12 +4,31 @@
 
 from pathlib import Path
 
+import requests
+
 ############# filepat exists #############
 
 def filepatExists(filepath: str) -> bool:
   p      = Path(filepath)
   result = any(p.parent.glob(p.stem + ".*")) #allow any extension
   return result
+
+############# filepat exists #############
+
+def downloadRemote(url: str, localPath: str) -> bool:
+  try:
+    #url = "https://example.com/path/to/image.png"
+    #local_path = "image.png"
+
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()  # Ensures HTTP errors raise an exception
+
+    with open(local_path, "wb") as f:
+      f.write(response.content)
+
+    if self.verbose: self.msg("Downloaded to " + localPath)
+  except:
+    self.err("downloadRemote")
 
 ### end ###
 
