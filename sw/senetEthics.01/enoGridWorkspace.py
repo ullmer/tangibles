c696e3c5169b68bef08e44583614176576f02e4e
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-27 00:14:19 -0400

    progress

--- sw/senetEthics.01/enoGridWorkspace.py
+++ sw/senetEthics.01/enoGridWorkspace.py
@@ -3,6 +3,13 @@
 # Begun 2026-03-27
 
 from enoSegmentedImgCoords import *
+from ataBase               import *
+from typing                import Any, List
 
+Matrix = List[List[Any]]
+
+class EnoGridWorkspace(AtaBase):
+  dimensions:    tuple[int,int]|None = None
+  gridWorkspace: Matrix|None         = None
 
 ### end ###
