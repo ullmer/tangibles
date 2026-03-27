cbfc503236db140fc3a46d13f8283ee761e13f03
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-27 00:09:31 -0400

    stub

---
+++ sw/senetEthics.01/enoGridWorkspace.py
@@ -0,0 +1,8 @@
+# Experiment in representing a gridded workspace (virtual or physical)
+# Brygg Ullmer, Clemson University
+# Begun 2026-03-27
+
+from enoSegmentedImgCoords import *
+
+
+### end ###
