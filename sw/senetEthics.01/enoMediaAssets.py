c0ae5416058880dbf88aa4012ca748b4cd44b509
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-28 17:24:47 -0400

    progress

--- sw/senetEthics.01/enoMediaAssets.py
+++ sw/senetEthics.01/enoMediaAssets.py
@@ -1,13 +1,15 @@
-# Enodia Backdrops
+# Enodia Media Assets
 # Brygg Ullmer, Clemson University
 # Begun 2026-03-28
 
 import yaml
+
+from ataBase         import *
 from enoOSsupport    import *
 
 ################### Enodia Media Asset ###################
 
-class EnoMediaAsset:
+class EnoMediaAsset(AtaBase):
   mediaSubpath    = "images/" # type: str|None
   mediaUrl        = None      # type: str|None
   mediaLocalpath  = None      # type: str|None
@@ -17,5 +19,9 @@ class EnoMediaAsset:
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
     super().__init__()
+
+################### Enodia Media Assets ###################
+
+class EnoMediaAssets(AtaBase):
 
 ### end ###
