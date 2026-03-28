725e50666c4c236b2b3737c7df70e65411b4eba1
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-28 16:14:47 -0400

    progress

--- sw/senetEthics.01/enoOSsupport.py
+++ sw/senetEthics.01/enoOSsupport.py
@@ -10,7 +10,7 @@ import requests
 
 def filepatExists(filepath: str) -> bool:
   p      = Path(filepath)
-  result = any(p.parent.glob(p.stem + ".*")) #allow any extension
+  result = any(p.parent.glob(p.stem + "*")) #allow any extension
   return result
 
 ############# filepat exists #############
@@ -20,13 +20,15 @@ def downloadRemote(url: str, localPath: str) -> bool:
     #url = "https://example.com/path/to/image.png"
     #local_path = "image.png"
 
+    if self.filepatExists(localPath): 
+      self.msg("downloadRemote: file present, ignoring"); return None
+
     response = requests.get(url, timeout=10)
     response.raise_for_status()  # Ensures HTTP errors raise an exception
 
-    with open(local_path, "wb") as f:
-      f.write(response.content)
+    with open(local_path, "wb") as f: f.write(response.content)
 
-    if self.verbose: self.msg("Downloaded to " + localPath)
+    if self.verbose: self.msg("downloaded to " + localPath)
   except:
     self.err("downloadRemote")
 
