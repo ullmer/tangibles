5a89f1c637d82e269a20ead26334999f901e869f
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-31 16:25:30 -0400

    stub

--- sw/senetEthics.01/tests/tstMediaAssets01.py
+++ sw/senetEthics.01/tests/tstMediaAssets01.py
@@ -9,4 +9,11 @@ ema1 = EnoMediaAsset
 url = "https://computing.clemson.edu/~bullmer/images/chessSofonisbaAnguissola1555o.jpg"
 lfn = "chessSofonisbaAnguissola1555o.jpg"
 
+mediaSubpath = "images/" # type: str|None
+  cacheSubpath = "cache/"  # type: str|None
+  mediaUrl     = None      # type: str|None
+  mediaFn      = None      # type: str|None
+  autoDLMedia  = True      # type: bool
+
+
 ### end ###
