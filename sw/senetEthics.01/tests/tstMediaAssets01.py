e87ac2f0bc1ad66ba4613157c17cadfdb34690b9
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-31 16:19:10 -0400

    stub

---
+++ sw/senetEthics.01/tests/tstMediaAssets01.py
@@ -0,0 +1,13 @@
+enoMediaAssets.py
+# Media assets description
+backdrops:
+  chessSA1:
+    url:       https://computing.clemson.edu/~bullmer/images/chessSofonisbaAnguissola1555o.jpg
+    copyright: https://en.wikipedia.org/wiki/Chess#/media/File:The_Chess_Game_(Sofonisba_Anguissola)_1555_(4096x3236px).jpg
+    copyrightNotes: Creative Commons Attribution-Share Alike 4.0 International : Mortendrak + Ullmer
+    local: chessSofonisbaAnguissola1555o.jpg
+
+  chessSA1:
+    local: senet03k.png
+
+### end ###
