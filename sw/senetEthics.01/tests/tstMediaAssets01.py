4dc56f8243dcce324c83790891f240d951d4ad09
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-31 16:20:29 -0400

    stub

--- sw/senetEthics.01/tests/tstMediaAssets01.py
+++ sw/senetEthics.01/tests/tstMediaAssets01.py
@@ -1,13 +1,12 @@
-enoMediaAssets.py
-# Media assets description
-backdrops:
-  chessSA1:
-    url:       https://computing.clemson.edu/~bullmer/images/chessSofonisbaAnguissola1555o.jpg
-    copyright: https://en.wikipedia.org/wiki/Chess#/media/File:The_Chess_Game_(Sofonisba_Anguissola)_1555_(4096x3236px).jpg
-    copyrightNotes: Creative Commons Attribution-Share Alike 4.0 International : Mortendrak + Ullmer
-    local: chessSofonisbaAnguissola1555o.jpg
+# Test of enodia media assets (01)
+# Brygg Ullmer, Clemson University
+# Begun 2026-03-31
 
-  chessSA1:
-    local: senet03k.png
+from enoMediaAssets import *
+
+ema1 = EnoMediaAsset
+
+url = "https://computing.clemson.edu/~bullmer/images/chessSofonisbaAnguissola1555o.jpg"
+lfn = "chessSofonisbaAnguissola1555o.jpg"
 
 ### end ###
