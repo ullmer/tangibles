12139d799d08efa3ef9aa3f51506cbb571d228af
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-03-31 22:48:28 -0400

    migrated downloading functionality to enoRemoteContent.py

--- sw/activeGrids.01/enoOSsupport.py
+++ sw/activeGrids.01/enoOSsupport.py
@@ -14,28 +14,5 @@ def filepatExists(filepath: str) -> bool:
   result = any(p.parent.glob(p.stem + "*")) #allow any extension
   return result
 
-############# filepat exists #############
-
-def downloadRemote(url: str, localPath: str) -> bool:
-  try:
-    #url = "https://example.com/path/to/image.png"
-    #local_path = "image.png"
-
-    if filepatExists(localPath): 
-      #self.msg("downloadRemote: file present, ignoring"); return None
-      print("downloadRemote: file present, ignoring"); return None
-
-    response = requests.get(url, timeout=10, verify=certifi.where())
-
-    response.raise_for_status()  # Ensures HTTP errors raise an exception
-
-    with open(local_path, "wb") as f: f.write(response.content)
-
-    #if self.verbose: self.msg("downloaded to " + localPath)
-  except:
-    #self.err("downloadRemote")
-    print("downloadRemote error:")
-    traceback.print_exc(); 
-
 ### end ###
 
