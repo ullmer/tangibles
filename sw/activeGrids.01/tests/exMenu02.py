05eb1b2d237c73fbd94963f7e391592b8c0a3829
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-04-08 19:29:06 -0400

    debugging module paths

---
+++ sw/activeGrids.01/tests/exMenu02.py
@@ -0,0 +1,14 @@
+# https://pygame-zero.readthedocs.io/en/stable/ptext.html
+# https://pythonprogramming.altervista.org/pygame-4-fonts/
+
+import sys; sys.path.append("py") # import modules from py/ subdirectory 
+from enoAnimistHomeMenu import *
+
+WIDTH, HEIGHT = 900, 900
+
+eahm = enoAnimistHomeMenu(yamlFn = "yaml/animistHomeMenu01.yaml", requestAnim=True)
+
+def draw(): screen.clear(); eahm.draw(screen)
+def on_mouse_down(pos):     eahm.on_mouse_down(pos)
+
+### end ###
