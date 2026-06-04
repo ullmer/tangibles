c5740cefbac678b1ae572baa972b04ce9b5ff5c9
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2026-06-04 16:23:51 -0400

    progress

--- sw/enoVu.01/py/enoVuTboxPgz.py
+++ sw/enoVu.01/py/enoVuTboxPgz.py
@@ -4,4 +4,35 @@
 
 from enoVuTbox import *
 
+class EnoVuTboxPgz(EnoVuTbox):
+  textColor  = (255, 255, 255)
+  textAlpha  = .7
+  fontName   = "barlow_condensed_extralight"
+  fontSize   = 32
+
+  ######## constructor ######## 
+
+  def __init__(self, width: int, height: int, textStr: str) -> None: 
+    super().__init__(width, height, textStr)
+
+  ######## drawText ######## 
+
+  def drawText(self, screen):
+
+    tstr = self.textStr
+
+    if tstr is None: 
+      if self.verbose: self.msg('drawText called, no text to be drawn')
+
+    fn, fs     = self.fontName, self.fontSize
+    tox1, toy1 = self.textOffset
+
+    bx, by  = self.basePos
+    x,  y   = bx+tox1, by+toy1
+    ta, tc  = self.textAlpha, self.textColor
+
+    screen.draw.text(tstr, pos=(x,y), alpha=ta, color=tc, 
+                     fontname=fn, fontsize=fs)
+
+
 ### end ###
